464fafea0fea7f0c6197669d9e0b36a9f331fd04
CMLTTN <cemaldim@gmail.com>
2023-03-17 12:40:32 +0300

    password

--- material-intro-starter/src/components/TextFieldComp.jsx
+++ material-intro-starter/src/components/TextFieldComp.jsx
@@ -1,15 +1,33 @@
 import { Stack, Typography } from "@mui/material";
-import  TextField  from "@mui/material/TextField";
+import TextField from "@mui/material/TextField";
 
 const TextFieldComp = () => {
   return (
     <div>
-    <Typography variant="h4" color="secondary.dark" align="center" m={4} >TEXT FIELD</Typography>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <Typography variant="h4" color="secondary.dark" align="center" m={4}>
+        TEXT FIELD
+      </Typography>
+      <TextField
+        id="email"
+        label="email"
+        type="email"
+        variant="outlined"
+        placeholder="Enter Your Email"
+        margin="normal"
+        required
+      />
+      <TextField
+        id="password"
+        label="password"
+        type="password"
+        variant="filled"
+        placeholder="Enter Password"
+        margin="normal"
+        required
+      />
       <TextField id="standard-basic" label="Standard" variant="standard" />
     </div>
   );
-}
+};
 
-export default TextFieldComp
+export default TextFieldComp;
