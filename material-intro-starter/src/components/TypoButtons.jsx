f9ed3c16a7e3a774ba959da809479c2149a10ef9
CMLTTN <cemaldim@gmail.com>
2023-03-17 09:53:14 +0300

    typography

--- material-intro-starter/src/components/TypoButtons.jsx
+++ material-intro-starter/src/components/TypoButtons.jsx
@@ -1,5 +1,16 @@
+import { Typography } from "@mui/material";
+
 const TypoButtons = () => {
-  return <div>TypoButtons</div>;
+  return <div>
+  <Typography 
+  variant="h4" 
+  align="center" 
+  mt={3}
+  color="primary"  
+   >
+    MUI Typo
+  </Typography>
+  </div>;
 };
 
 export default TypoButtons;
