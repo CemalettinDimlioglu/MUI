bc55913200fb4d080680f2807dc5383bc18b55f3
CMLTTN <cemaldim@gmail.com>
2023-03-17 12:00:58 +0300

    typo

--- material-intro-starter/src/components/TypoButtons.jsx
+++ material-intro-starter/src/components/TypoButtons.jsx
@@ -1,25 +1,41 @@
-import { Container, Typography } from "@mui/material";
-import { color } from "@mui/system";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { color } from "@mui/material";
 
 const TypoButtons = () => {
   return (
-    <Container maxWidth="sm" >
-      <Typography variant="h4" align="center" mt={3} color="forestgreen">
-        MUI Typo
-      </Typography>
-      <Typography 
-      variant="body2" 
-      align="justify" 
-      mt={4}
-      sx={{backgroundColor: "red", color: "white",fontSize:"1.5rem"}}
-       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, impedit atque! Dicta sequi est magni incidunt adipisci. Ipsum architecto rerum eveniet sunt reprehenderit. Perferendis, veniam ducimus. Adipisci alias officiis quo.
-      </Typography>
-      <Typography variant="button" >
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit nisi exercitationem ratione voluptates, eos quaerat corrupti, facere veniam, quia ut obcaecati similique dolorem saepe velit.
+    <>
+      <Container maxWidth="sm">
+        <Typography variant="h4" align="center" mt={3} color="forestgreen">
+          MUI Typo
+        </Typography>
+        <Typography
+          variant="body2"
+          align="justify"
+          mt={4}
+          sx={{ backgroundColor: "red", color: "white", fontSize: "1.5rem" }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+          impedit atque! Dicta sequi est magni incidunt adipisci. Ipsum
+          architecto rerum eveniet sunt reprehenderit. Perferendis, veniam
+          ducimus. Adipisci alias officiis quo.
+        </Typography>
+        <Typography variant="button">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit nisi
+          exercitationem ratione voluptates, eos quaerat corrupti, facere
+          veniam, quia ut obcaecati similique dolorem saepe velit.
+        </Typography>
+      </Container>
+      <Container  >
+      <Box sx={{ marginTop: "1rem", display: "flex", justifyContent: "center", gap: 2}}>
+       <Button variant="text" color="warning">Text</Button>
+        <Button variant="contained" color="error">Contained</Button>
+        <Button variant="outlined" color="secondary">Outlined</Button>
+        <Button variant="outlined"  >Primary</Button>
+        </Box>
 
-      </Typography>
-    </Container>
+       
+      </Container>
+    </>
   );
 };
 
