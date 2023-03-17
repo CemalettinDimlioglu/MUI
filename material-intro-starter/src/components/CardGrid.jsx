d7ffa49f1654d74242632aedbf9df157e132b6a5
CMLTTN <cemaldim@gmail.com>
2023-03-17 16:19:38 +0300

    around finish

--- material-intro-starter/src/components/CardGrid.jsx
+++ material-intro-starter/src/components/CardGrid.jsx
@@ -3,57 +3,38 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions, Grid } from "@mui/material";
-import  data from "../data"
+import data from "../data";
 
 export default function CardGrid() {
-  return ( 
-  <>
-     <Typography 
-      variant="h4" 
-      align="center" 
-      m={3} 
-      color="red"
-        >
-      
-    
+  return (
+    <>
+      <Typography variant="h4" align="center" m={3} color="red">
         Card Grid
       </Typography>
-    <Grid container>
-     {data.map(()=>{
-    return (
-     <Grid item >
- <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-        </CardActions>
-      </Card>
-
-     </Grid>
-    )
-
-     })}
-    </Grid>
-
-     
-      </>
+      <Grid container justifyContent="center" alignItems="center" spacing={3}>
+        {data.map(({ id, name, img, text }) => (
+          <Grid item key={id} xs={12} sm={6} md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia component="img" image={img} alt="green iguana" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {text}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
