693fd0278807e02af6875e8281a6e5bfeb8df740
CMLTTN <cemaldim@gmail.com>
2023-03-17 12:29:39 +0300

    container

--- material-intro-starter/src/components/TextFieldComp.jsx
+++ material-intro-starter/src/components/TextFieldComp.jsx
@@ -1,8 +1,10 @@
+import { Stack, Typography } from "@mui/material";
 import  TextField  from "@mui/material/TextField";
 
 const TextFieldComp = () => {
   return (
     <div>
+    <Typography variant="h4" color="secondary.dark" align="center" m={4} >TEXT FIELD</Typography>
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       <TextField id="filled-basic" label="Filled" variant="filled" />
       <TextField id="standard-basic" label="Standard" variant="standard" />
