31d2041676db373b8954c7950d044948037833a8
CMLTTN <cemaldim@gmail.com>
2023-03-17 17:54:18 +0300

    finish

--- material-intro-starter/src/components/AppBarComp.jsx
+++ material-intro-starter/src/components/AppBarComp.jsx
@@ -11,7 +11,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import AppleIcon from "@mui/icons-material/Apple";
 
 const pages = ["Products", "Pricing", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -36,10 +36,10 @@ function AppBarComp() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static"  sx={{backgroundColor:"secondary.main"}} >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <AppleIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -55,7 +55,7 @@ function AppBarComp() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            CLARUSWAY
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -94,25 +94,7 @@ function AppBarComp() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
+
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
--- material-intro-starter/src/pages/Home.jsx
+++ material-intro-starter/src/pages/Home.jsx
@@ -2,16 +2,28 @@ import AppBarComp from "../components/AppBarComp";
 import CardGrid from "../components/CardGrid";
 import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { pink } from "@mui/material/colors";
 
 const Home = () => {
+  const theme = createTheme({
+palette :{
+  primary:{
+    main:"#bede",
+  },
+  secondary:{
+    main:pink[500],
+  }
+},
+  })
   return (
-    <>
+    <ThemeProvider theme={theme} >
       {/* <TypoButtons />
       <TextFieldComp/> */}
       <AppBarComp/>
       <CardGrid/>
 
-    </>
+    </ThemeProvider>
   );
 };
 
